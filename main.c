9dad12f0d7f0e8fa0045c0f45f35bd4d9814a74e
Gabrielle <gcspence@aggies.ncat.edu>
2021-09-14 14:42:35 -0400

    interrupts

--- main.c
+++ main.c
@@ -10,37 +10,32 @@ void init_uart(void){
     UCA0BR1 = 0;
     UCA0MCTL = UCBRS_1;
     UCA0CTL1 &= ~UCSWRST;
-    IE2 = UCA0RXIE;
+    IE2 |= UCA0RXIE;
 
 }
 
 void init_i2c(void)
 {
-    UCB0CTL1 |= UCSWRST;
-    UCB0CLT0 |= UCMST|UCMODE_3|UCSYNC;
+    UCB0CTL1 |= UCSWRST;                    //Software reset
+    UCB0CLT0 |= UCMST|UCMODE_3|UCSYNC;      //Set sync communication, set as master, and put in i2c mode
     UCB0CLT1 |= UCSSEL_2|UCSWRST;
     UCB0BR0 = 10;
     UCB0BR1 = 0;
-    UCB0I2CSA = 0x48;
-    UCB0CTL &= ~UCSWRST;
-    IE2 |= UCB0TXIE;
+    UCB0CTL &= ~UCSWRST;                    //Software reset disabled
+    IE2 |= UCB0TXIE;                        //transmit interrupt enabled
 
 }
 
-void transmit(void)
-{
-
-}
-
-void receive(void)
-{
-
+void clk(void){
+    /*
+     * figure out how to set up clock to stagger readings
+     */
 }
 
 void port_setup(void)
 {
    P1DIR |= BIT5;
-   P1SEL |= BIT1|BIT2|BIT6|BIT7;
+   P1SEL |= BIT1|BIT2|BIT6|BIT7;  //P1.1 = RxD P1.2 = TxD P1.6 = SCL P1.7 = SDA
    P1SEL2 |= BIT1|BIT2|BIT6|BIT7;
    P1OUT &= ~BIT5;
    P1OUT |= BIT5;
@@ -56,8 +51,33 @@ void port_setup(void)
 int main(void)
 {
     WDTCTL = WDTPW | WDTHOLD;       // stop watchdog timer
+    port_setup();
+    init_i2c();
+    init_uart();
 
+    while(1){
+        UBB0CLT1 |= UCTR;                           //put in transmit mode
+        UCB0CLT1 |= UCTXSTT;                        //start transmission
 
+        while((UCB0STAT & UCSTIFG) == 0){           //while transmission interrupt flag is not raised clear flag
+            UCB0STAT &= ~UCSTIFG;
+        }
+
+        UBB0CLT1 &= ~UCTR;                          //put in receive mode
+        UCB0CLT1 |= UCTXSTT;                        //start transmission
+
+        while((UCB0STAT & UCSTIFG) == 0){           //while transmission interrupt flag is not raised clear flag
+            UCB0STAT &= ~UCSTIFG;
+        }
+
+    }
 
     return 0;
 }
+
+
+#pragma vector = USCIAB0RX VECTOR
+__interrupt void transmit(void){
+
+}
+
